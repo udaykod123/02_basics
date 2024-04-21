d22013215038c7aa37dd960dda2e6b5b140b9bd1
uday mishra <135223377+udaykod123@users.noreply.github.com>
2024-04-21 21:50:00 +0000

    arrays config

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+const myArr = [0, 1, 2 ,3, 4, 5 ]
+
+const myHeros = ["RatanTata", "RohitSharma", "AkashPandey"]
+// console.log(myArr[1]);
+// console.log(myHeros[0]);
+// myArr.push(10)
+// myArr.pop()
+// myArr.unshift(7)
+// myArr.shift()
+// console.log(myArr.includes(4));
+// console.log(myArr.indexOf(5));
+
+const newArr = myArr.join()
+console.log(myArr);
+console.log(newArr);
